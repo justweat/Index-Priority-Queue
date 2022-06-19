0bfa96c73ec0c1e7d8de3320370e8366b4d9c430
justweat <justinweathers44@gmail.com>
2022-06-18 21:32:59 -0400

    condensed into header for portability. main currently acting as unit test. initial tests succeeded.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <thread>
+#include <chrono>
 #include "IndexPriorityQueue.h"
 
 using namespace std;
 
 int main() {
 
-    vector<int> keys{1,2,3,4,5,6,7,8,9,10,11};
-    vector<double> vals{4.4,10.10,6.6,2.2,1.1,7.7,8.8,9.9,3.3,5.5,-2.2};
+    vector<int> keys{};
+    vector<double> vals{};
 
     IndexPQ::IndexPriorityQueue<int, double> pq(keys, vals, IndexPQ::IndexPQType{IndexPQ::IndexPQType::MinHeap});
 
-    pq.updateKey(2, -1);
-    pq.updateKey(11, 500);
-    pq.push(12, -5);
-    pq.push(13, 3.4);
-    pq.updateKey(13, -10);
-    pq.push(14, 5.6);
-    pq.updateKey(14, -1000);
-    pq.updateKey(14, 1000);
+    double lower_bound = -10000;
+    double upper_bound = 10000;
+    std::uniform_real_distribution<double> random_values(lower_bound,upper_bound);
+    std::default_random_engine re(chrono::system_clock::now().time_since_epoch().count());
 
-    cout << pq.keyValue(3) << endl;
 
-    while(!pq.empty()){
-        pq.printIPQ();
-        pq.pop();
-        cout << "\n";
+    for(int i = 0; i < 20000; ++i){
+
+        double a_random_double = random_values(re);
+        pair<int, double> t{i, a_random_double};
+        pq.push(t.first, t.second);
+
+//        pq.printIPQ();
+//        cout << "\n";
+
+        std::uniform_int_distribution<int> random_indices(0,i);
+        int index = random_indices(re);
+        a_random_double = random_values(re);
+        pq.updateKey(index, a_random_double);
+
+//        pq.printIPQ();
+//        cout << "\n";
+
+//        this_thread::sleep_for(chrono::seconds(2));
+
+        auto tempPQ = pq;
+        vector<double> val_test{};
+        while(!tempPQ.empty()){
+            val_test.emplace_back(tempPQ.frontValue());
+            tempPQ.pop();
+        }
+
+        for(size_t j{1}; j < val_test.size(); ++j){
+            if(val_test[j] < val_test[j - 1]){
+                throw logic_error("Invalid heap");
+            }
+        }
+
+        cout << "Passed... " << pq.size() << "\n";
+
     }
 
     return 0;
